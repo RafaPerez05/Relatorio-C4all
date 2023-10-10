c62150076057856a3677636ede7df983a466b4f6
Yan0606 <yan060604@gmail.com>
2023-10-10 14:59:31 -0300

    adicionando nomes ficticios

--- client/src/components/DropdownAlunos.vue
+++ client/src/components/DropdownAlunos.vue
@@ -1,23 +1,41 @@
 <template>
-    <div class="select is-fullwidth">
-        <select v-model="selectedStudent" @change="handleSelectChange">
-        <option value="">Selecione um aluno</option>
-        <option v-for="student in students" :key="student" :value="student">{{ student }}</option>
+<div class="select is-fullwidth is-multiple">
+  <b-quickview >
+    <b-quickview-trigger>
+      <select v-model="selectedStudents" @change="handleSelectChange">
+        <option v-for="student in students" :key="student.id" :value="student.id">{{ student.name }}</option>
       </select>
-    </div>
-  </template>
-  
-  <script>
+    </b-quickview-trigger>
+  </b-quickview>
+</div>
 
-  export default {
-    data() {
-      
-      return {
-        selectedStudent: "",
-      };
-    },
-    methods: {
+</template>
 
+<script>
+export default {
+  data() {
+    return {
+      selectedStudents: [],
+      students: [
+        { id: 1, name: "João" },
+        { id: 2, name: "Maria" },
+        { id: 3, name: "Pedro" },
+        { id: 4, name: "Ana" },
+        // alunos fictícios aqui
+      ],
+    };
+  },
+  methods: {
+    handleSelectChange() {
+      // Lógica para lidar com a seleção dos alunos
+      // Você pode acessar os alunos selecionados através de this.selectedStudents
+      // Exemplo de lógica:
+      console.log("Alunos selecionados:", this.selectedStudents);
     },
-  };
-  </script>
+    toggleDropdown() {
+      // Esta função é chamada quando o botão do dropdown é clicado
+      // Não é necessário definir showStudents aqui
+    },
+  },
+};
+</script>
